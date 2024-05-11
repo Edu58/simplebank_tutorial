580818942bfe284b4aa58465c65e6afc791aaf64
Edu58 <edumuriithi58@gmail.com>
2024-05-11 14:11:04 +0300

    feat: add Gin Accounts REST API

---
+++ api/account.go
@@ -0,0 +1,95 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/techschool/simplebank/db/sqlc"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"oneof=USD EUR KES"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var request createAccountRequest
+	if err := ctx.ShouldBindJSON(&request); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	args := db.CreateAccountParams{
+		Owner:    request.Owner,
+		Currency: request.Currency,
+		Balance:  0,
+	}
+
+	account, err := server.store.CreateAccount(ctx, args)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var accountID getAccountRequest
+
+	if err := ctx.ShouldBindUri(&accountID); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, accountID.ID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type listAccountsRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=1,max=10"`
+}
+
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var pageOptions listAccountsRequest
+
+	if err := ctx.ShouldBindQuery(&pageOptions); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	args := db.ListAccountsParams{
+		Limit:  pageOptions.PageSize,
+		Offset: (pageOptions.PageID - 1) * pageOptions.PageSize,
+	}
+
+	account, err := server.store.ListAccounts(ctx, args)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
